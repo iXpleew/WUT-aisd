13b739968595d4cb6df4f553a02b3fbb5b92def1
Oskar Sałata <Xpleew16@gmail.com>
2026-04-03 14:22:46 +0200

    spliting sides

--- sorting_algorithm/main.py
+++ sorting_algorithm/main.py
@@ -4,6 +4,17 @@ word_list = []
 
 
 def merge_sort(words: list[str]) -> list[str]:
+    list_size = len(words)
+    if list_size <= 1:
+        return words
+    
+    mid = list_size // 2
+    left_part = words[:mid]
+    right_part = words[mid:]
+
+    sort_left = merge_sort(left_part)
+    sort_right = merge_sort(right_part)
+
     return []
 
 with open("input.txt", "r") as file:
