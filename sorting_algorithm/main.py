bf48431a9cd6017185e762a663e38d2098719770
Oskar Sałata <Xpleew16@gmail.com>
2026-04-03 12:51:41 +0200

    started to work on a merge sort

--- sorting_algorithm/main.py
+++ sorting_algorithm/main.py
@@ -2,6 +2,10 @@ MAX_MEMORY = 1000
 counter = 1
 word_list = []
 
+
+def merge_sort(words: list[str]) -> list[str]:
+    return []
+
 with open("input.txt", "r") as file:
     for i in range(MAX_MEMORY):
         word_list.append(file.read())
