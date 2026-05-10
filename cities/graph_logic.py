5554a567bad974df2709abfd699c311c422b18de
Oskar Sałata <Xpleew16@gmail.com>
2026-05-10 14:57:16 +0200

    imported pprint to show all of the tree

---
+++ cities/graph_logic.py
@@ -0,0 +1,9 @@
+from destination import Destination
+
+def create_destiantion(path: dict, source: str, dest: str, distance: float, morning: int, noon: int, evening:int):
+    new_destination = Destination(dest, distance, morning, noon, evening)
+    if source in path:
+        path[source].append(new_destination)
+    else:
+        new_paths_list = [new_destination]
+        path[source] = new_paths_list
