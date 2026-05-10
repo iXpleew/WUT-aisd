645e7e8f1d77bd79a12509d4ba94e191a66cb79b
Oskar Sałata <Xpleew16@gmail.com>
2026-05-10 16:22:15 +0200

    started to work on a dijkstra algorithm

--- cities/graph_logic.py
+++ cities/graph_logic.py
@@ -1,5 +1,6 @@
 from destination import Destination
 
+
 def create_destiantion(path: dict, source: str, dest: str, distance: float, morning: int, noon: int, evening:int):
     new_destination = Destination(dest, distance, morning, noon, evening)
     if source in path:
@@ -7,3 +8,7 @@ def create_destiantion(path: dict, source: str, dest: str, distance: float, morn
     else:
         new_paths_list = [new_destination]
         path[source] = new_paths_list
+
+
+def calculate_dijkstra_distance(map:dict, start_location: str, final_location: str):
+    pass
