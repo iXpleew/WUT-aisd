ec302dc89461030c10dd6219780a8c0278b71346
Oskar Sałata <Xpleew16@gmail.com>
2026-05-13 21:35:11 +0200

    started to wokr on seperating methods

--- cities/graph_logic.py
+++ cities/graph_logic.py
@@ -13,6 +13,7 @@ def create_destiantion(path: dict, source: str, dest: str, distance: float, morn
         path[source] = new_paths_list
 
 
+#tutaj bedzie sparsowana kategoria
 def define_dijkstra_type(category: int, city: Destination) -> float:
     if category < 0:
         return city.length
@@ -24,51 +25,60 @@ def define_dijkstra_type(category: int, city: Destination) -> float:
         return city.evening_time
     else:
         return city.night_time()
-    
-    
-    match category:
-        case "distance":
-            return city.length
-        case "morning":
-            return city.morning_time
-        case "noon":
-            return city.noon_time
-        case "evening":
-            return city.evening_time
-        case "night":
-            return city.night_time()
-    return 0.
+
+# make this method calculate time 
+def calculate_proper_time(current_time: str, add_time: float):
+    hour_time = int(current_time[:2])
+    minutes_time = int(current_time[3:])
+    while add_time > 0:
+        minutes_to_full_hour = 60 - minutes_time
+        if add_time > minutes_to_full_hour:
+            if hour_time == 23:
+                hour_time = 0
+            else:
+                hour_time += 1
+            add_time -= minutes_to_full_hour
+            minutes_time = 0
+        else:
+            minutes_time += add_time
+    return f"{hour_time:02d}:{minutes_time:02d}"
 
 
-def calculate_dijkstra_distance(map:dict, start_location: str, final_location: str, by_what: str):
+
+def calculate_dijkstra_distance(map: dict, start_location: str, final_location: str):
     distances_from_start = {}
     for key in map:
         if key == start_location:
-            distances_from_start[key] = 0
+            distances_from_start[key] = 0.0
         else:
-            distances_from_start[key] = np.inf
+            distances_from_start[key] = float('inf')
 
     queue = [(0.0, start_location)]
     while queue:
-      current_distance, current_node = heapq.heappop(queue)
-      if current_distance > distances_from_start[current_node]:
-          continue
-      
-      for neighbour in map[current_node]:
-        distance = current_distance + define_dijkstra_type(by_what, neighbour)
-        if distance < distances_from_start[neighbour.destination]:
-            distances_from_start[neighbour.destination] = distance
-            heapq.heappush(queue, (distance, neighbour.destination))
+        current_distance, current_node = heapq.heappop(queue)
+        
+        if current_distance > distances_from_start[current_node]:
+            continue
+        
+        for neighbour in map[current_node]:
+            distance = current_distance + neighbour.lenght
+            if distance < distances_from_start[neighbour.destination]:
+                distances_from_start[neighbour.destination] = distance
+                heapq.heappush(queue, (distance, neighbour.destination))
     
     return distances_from_start[final_location]
 
 
-def get_dijsktra_info(city_number: int, map: dict, by_what: str) -> list[tuple]:
+def calculate_dijkstra_time(map:dict, start: str, final: str, current_time: str):
+    pass
+
+
+def get_dijsktra_info(city_number: int, map: dict, curr_time: int) -> list[tuple]:
     list_of_info = []
     for i in range(10):
         start_city = str(random.randint(0, city_number))
         end_city = str(random.randint(0, city_number))
-        distance = calculate_dijkstra_distance(map, start_city, end_city, by_what)
+        distance = calculate_dijkstra_distance(map, start_city, end_city, curr_time)
         list_of_info.append((start_city, end_city, distance))
     return list_of_info
 
